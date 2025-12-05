c7b32d25ef2480d870f4d14c6cff56b23d3a5626
Muskaan Singh <muskaan.singh@prepladder.com>
2025-12-05 17:48:01 +0530

    Implemented cart functionality

--- src/components/CartListing.jsx
+++ src/components/CartListing.jsx
@@ -1,44 +1,107 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { removeItem } from "../redux/slice";
+
 const CartListing = () => {
+  const cartSelector = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch();
+  const [cartItems, setCartItems] = useState(cartSelector);
+
+  useEffect(() => {
+    setCartItems(cartSelector);
+  }, [cartSelector]);
+
+  console.log("cartSelector", cartSelector);
+
+  // Calculate total price
+  const totalPrice = cartItems.reduce(
+    (acc, item) =>
+      item.quantity ? (acc + item.price) * item.quantity : acc + item.price,
+    0
+  );
+
+  const manageQuantity = (id, qty) => {
+    // let quantity = qty > 1 ? qty : 1;
+    let quantity = parseInt(qty) > 1 ? parseInt(qty) : 1; // just to ensure qty is an integer
+
+    const cartTempItems = cartItems.map((item) => {
+      return item.id === id ? { ...item, quantity } : item; // enter quantity in the item
+    });
+
+    setCartItems(cartTempItems);
+  };
+
   return (
     <div className="cart-page">
-      <div className="cart-page-title">Your Cart Items</div>
+      <div className="cart-page-title">
+        <h2> Your Cart Items</h2>
+        <div>({cartItems.length} items)</div>
+      </div>
 
       <div className="cart-list">
-        <div className="cart-item">
-          <div className="thumb">A</div>
-          <div className="item-info">
-            <div className="item-title">Wireless Headphones</div>
-            <div className="item-meta">Qty 1 · Black</div>
-          </div>
-          <div className="price">₹2,199</div>
-        </div>
+        {cartItems && !cartItems.length ? (
+          <p style={{ padding: 20, textAlign: "center", color: "#666" }}>
+            Your cart is empty.
+          </p>
+        ) : (
+          cartItems &&
+          cartItems.map((item) => (
+            <div key={item.id} className="cart-item">
+              <img src={item.thumbnail} className="thumb" alt={item.title} />
 
-        <div className="cart-item">
-          <div className="thumb">B</div>
-          <div className="item-info">
-            <div className="item-title">Portable Charger</div>
-            <div className="item-meta">Qty 1 · 20,000 mAh</div>
-          </div>
-          <div className="price">₹1,099</div>
-        </div>
+              <div className="item-info">
+                <div className="item-title">{item.title}</div>
+                <div className="item-meta">Qty 1</div>
+              </div>
+
+              <div className="item-actions">
+                <input
+                  type="number"
+                  placeholder="Enter Quantity"
+                  className="qty-input"
+                  onChange={(e) => manageQuantity(item.id, e.target.value)}
+                  value={item.quantity ? item.quantity : 1}
+                />
+                <div className="qty-price">
+                  $
+                  {(item.quantity
+                    ? item.price * item.quantity
+                    : item.price
+                  ).toFixed(2)}
+                </div>
+                <button
+                  onClick={() => dispatch(removeItem(item))}
+                  className="remove-item"
+                >
+                  Remove
+                </button>
+              </div>
+            </div>
+          ))
+        )}
       </div>
 
-      <div className="cart-summary">
-        <div className="cart-summary-total">Total: ₹3,298</div>
+      {cartItems.length > 0 && (
+        <div className="cart-summary">
+          <div className="cart-summary-total">
+            Total: ${totalPrice.toFixed(2)}
+          </div>
 
-        <div className="cart-summary-buttons">
-          <button
-            className="view-cart"
-            onClick={() => alert("Continue shopping")}
-          >
-            Continue Shopping
-          </button>
+          <div className="cart-summary-buttons">
+            <Link to="/">
+              <button className="view-cart">Continue Shopping</button>
+            </Link>
 
-          <button className="checkout" onClick={() => alert("Checkout")}>
-            Checkout
-          </button>
+            <button
+              className="checkout"
+              onClick={() => alert("Proceed to Checkout")}
+            >
+              Checkout
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
